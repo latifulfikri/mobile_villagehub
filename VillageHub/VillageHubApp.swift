da79c5cffc09eb807a108dc45e95f16c795dee02
latifulfikri <latifulfikri1902@gmail.com>
2023-02-18 18:44:34 +0700

    Initial Commit

---
+++ VillageHub/VillageHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  VillageHubApp.swift
+//  VillageHub
+//
+//  Created by Fikri Yuwi on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct VillageHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
